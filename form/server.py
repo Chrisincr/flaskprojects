6b8f425761bb139560baf9c635261ba19b1686cc
Chris Thompson <chrisincr@gmail.com>
2019-02-07 21:14:14 -0600

    finished Flask form assignment

--- form/server.py
+++ form/server.py
@@ -8,8 +8,14 @@ def index():
 
 @app.route('/results', methods=['POST'])
 def show_submission():
-    print({request.form})
-    return render_template("submission.html",name=request.form["name"],email=request.form['email'],subscribe=request.form['subscribe'])
+    print(request.form)
+    
+    subscribe = request.form.get('subscribe','No')
+    if subscribe == 'checkedValue':
+        subscribe = 'Yes'
+
+    
+    return render_template("submission.html",name=request.form["name"],email=request.form['email'],subscribe=subscribe)
 
 
 
